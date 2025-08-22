147dd022637f21819a6879f06ab8d5e92e66916e
krosoft <11226227+krosoft-dev@users.noreply.github.com>
2025-08-22 18:48:22 +0200

    Ajout EnumerableExtensions (#9)

---
+++ src/Krosoft.Extensions.Mapping/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Krosoft.Extensions.Core.Extensions;
+using Krosoft.Extensions.Core.Models;
+
+namespace Krosoft.Extensions.Mapping.Extensions;
+
+/// <summary>
+/// Méthodes d'extensions pour les classes implémentant l'interface <see cref="IEnumerable{T}" />.
+/// </summary>
+public static class EnumerableExtensions
+{
+    public static IEnumerable<TDestination> MapTo<TSource, TDestination>(this IEnumerable<TSource> items,
+                                                                         IMapper mapper) =>
+        mapper.Map<IEnumerable<TDestination>>(items);
+
+    public static PaginationResult<TDestination> ToPagination<TSource, TDestination>(this IEnumerable<TSource> items,
+                                                                                     ISearchPaginationRequest paginationRequest,
+                                                                                     IMapper mapper)
+    {
+        var list = items.ToList();
+
+        var pagined = list
+                      .MapTo<TSource, TDestination>(mapper)
+                      .SortBy(paginationRequest)
+                      .Skip((paginationRequest.PageNumber - 1) * paginationRequest.PageSize)
+                      .Take(paginationRequest.PageSize);
+
+        return new PaginationResult<TDestination>(pagined, list.Count, paginationRequest.PageNumber, paginationRequest.PageSize);
+    }
+}
+ 
---
+++ tests/Krosoft.Extensions.Mapping.Tests/Extensions/EnumerableExtensionsTests.cs
@@ -0,0 +1,65 @@
+﻿using System.Reflection;
+using AutoMapper;
+using Krosoft.Extensions.Core.Models;
+using Krosoft.Extensions.Mapping.Extensions;
+using Krosoft.Extensions.Samples.Library.Mappings;
+using Krosoft.Extensions.Samples.Library.Models;
+using Krosoft.Extensions.Samples.Library.Models.Dto;
+using Krosoft.Extensions.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Krosoft.Extensions.Mapping.Tests.Extensions;
+
+[TestClass]
+public class EnumerableExtensionsTests : BaseTest
+{
+    private IMapper _mapper = null!;
+
+    protected override void AddServices(IServiceCollection services, IConfiguration configuration)
+    {
+        var all = new List<Assembly> { typeof(CompteProfile).Assembly };
+        services.AddAutoMapper(all);
+    }
+
+    [TestMethod]
+    public void MapToTest()
+    {
+        var source = new List<Compte>
+        {
+            new Compte { Name = "Test_1" },
+            new Compte { Name = "Test" }
+        };
+
+        var dest = source.MapTo<Compte, CompteDto>(_mapper).ToList();
+
+        Check.That(dest).HasSize(2);
+        Check.That(dest.Select(x => x.Name)).ContainsExactly("Test_1", "Test");
+    }
+
+    [TestMethod]
+    public void ToPaginationTest()
+    {
+        var source = new List<Compte>
+        {
+            new Compte { Name = "Test_1" },
+            new Compte { Name = "Test" }
+        };
+
+        var paginationResult = source.ToPagination<Compte, CompteDto>(new PaginationRequest(), _mapper);
+
+        Check.That(paginationResult.PageNumber).IsEqualTo(1);
+        Check.That(paginationResult.PageSize).IsEqualTo(10);
+        Check.That(paginationResult.TotalCount).IsEqualTo(2);
+        Check.That(paginationResult.TotalPages).IsEqualTo(1);
+        Check.That(paginationResult.Items).HasSize(2);
+        Check.That(paginationResult.Items.Select(x => x.Name)).ContainsExactly("Test_1", "Test");
+    }
+
+    [TestInitialize]
+    public void SetUp()
+    {
+        var serviceProvider = CreateServiceCollection();
+        _mapper = serviceProvider.GetRequiredService<IMapper>();
+    }
+}
